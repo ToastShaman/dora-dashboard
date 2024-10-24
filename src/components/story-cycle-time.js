63a60dfc0d97475c2ad6897f5889332382276912
Denver, Kevin <Kevin.Denver@zuhlke.com>
2024-10-24 14:10:05 +0100

    Add date-fns

--- src/components/story-cycle-time.js
+++ src/components/story-cycle-time.js
@@ -1,13 +1,15 @@
 import * as Plot from "npm:@observablehq/plot";
 import { ascending, quantile, min, max } from "d3-array";
 import { utcTicks, utcMonth } from "d3-time";
+import { differenceInHours, formatDistance } from 'date-fns'
 
 export function cycleTime(stories, { threshold, width, height } = {}) {
-    const diffTime = (d) => Math.abs(new Date(d.resolved) - new Date(d.created)) / 360000;
+    const diffTime = (d) => differenceInHours(new Date(d.resolved), new Date(d.created));
+    const fmtDiffTime = (d) => formatDistance(new Date(d.resolved), new Date(d.created), { addSuffix: true });
     const storiesWithCycleTime = stories.map(d => ({ ...d, cycle_time_h: diffTime(d) }));
     const percentile95 = quantile(storiesWithCycleTime.map(d => d.cycle_time_h).sort(ascending), 0.95);
     const percentile80 = quantile(storiesWithCycleTime.map(d => d.cycle_time_h).sort(ascending), 0.80);
-    
+
     const minDate = min(stories, d => new Date(d.created));
     const maxDate = max(stories, d => new Date(d.resolved));
     const ticks = utcTicks(minDate, maxDate, utcMonth.every(1));
@@ -29,14 +31,14 @@ export function cycleTime(stories, { threshold, width, height } = {}) {
         marks: [
             Plot.ruleY([percentile95], {
                 stroke: "lightcoral",
-                strokeWidth: 2,      
+                strokeWidth: 2,
                 strokeDasharray: "4, 4",
                 label: "95th Percentile"
             }),
 
             Plot.ruleY([percentile80], {
                 stroke: "lightcoral",
-                strokeWidth: 2,      
+                strokeWidth: 2,
                 strokeDasharray: "4, 4",
                 label: "80th Percentile"
             }),
@@ -52,7 +54,7 @@ export function cycleTime(stories, { threshold, width, height } = {}) {
             Plot.text(storiesWithCycleTime, {
                 x: "created",
                 y: "cycle_time_h",
-                text: d => `${d.id} (${d.status})`,
+                text: d => `${d.id} (${d.status}) - ${fmtDiffTime(d)}`,
                 lineAnchor: "bottom",
                 dy: -10,
                 lineWidth: 10,
